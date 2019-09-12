06e21a85816c5e769e8a41626168bec9678d6eb4
tedglim <tedlim@alumni.stanford.edu>
2019-09-11 20:52:00 -0700

    Tile Placement chagne
    CL
    improved ux on tile placement

--- Assets/Scripts/AStar.cs
+++ Assets/Scripts/AStar.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections;
+﻿using System.Net.Mime;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public enum TileType {GRASS, PATH, WATER, START, END}
 
@@ -20,29 +23,51 @@ public class AStar : MonoBehaviour
 
     [SerializeField]
     private Tilemap tilemap;
+
+    private TileButton prevButton;
+    private int initTileType = -1;
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit2D hit = Physics2D.Raycast(mainCam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, mask);
-        if (hit.collider != null)
-        {
-            Vector3 mouseWorldPos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-            Vector3Int clickPos = tilemap.WorldToCell(mouseWorldPos);
-            ChangeTile(clickPos);
-        }
-        
+            if (hit.collider != null)
+            {
+                Vector3 mouseWorldPos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+                Vector3Int clickPos = tilemap.WorldToCell(mouseWorldPos);
+                ChangeTile(clickPos);
+            }
         }
     }
 
     public void ChangeTileType(TileButton button)
     {
-        tileType = button.myTileType;
+        Color buttonColor = button.GetComponent<Image>().color;
+        if (buttonColor == Color.white)
+        {
+            button.GetComponent<Image>().color = Color.red;
+            tileType = button.myTileType;
+            initTileType = (int)tileType;
+            if (prevButton != null)
+            {
+                prevButton.GetComponent<Image>().color = Color.white;
+            }
+            prevButton = button;
+        }
     }
 
     private void ChangeTile(Vector3Int clickPos)
     {
-        tilemap.SetTile(clickPos, tiles[(int)tileType]);
+        Tile tileToChange = (Tile)tilemap.GetTile(clickPos);
+        String nameOfTileToChange = (tileToChange.sprite.name);
+        // if(tileType == TileType.START) {
+        //     print("start tile");
+        // }
+        if (nameOfTileToChange != "endTile" && nameOfTileToChange != "startTile" && nameOfTileToChange != "Water" && initTileType != -1)
+        {
+            tilemap.SetTile(clickPos, tiles[(int)tileType]);
+        } else {
+            print("Cannot place tile here.");
+        }
     }
-
 }
